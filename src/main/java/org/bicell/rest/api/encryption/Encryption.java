9c784def6f54b91a811fdad7b6a61255091f6edf
Anil Can OZGOK <ozgok1903@gmail.com>
2022-10-02 23:49:03 +0300

    Create Encryption.java
    encryption algorithm will be implemented later

---
+++ src/main/java/org/bicell/rest/api/encryption/Encryption.java
@@ -0,0 +1,9 @@
+package org.bicell.rest.api.encryption;
+
+public class Encryption {
+
+    //TODO implement encryption algorithm
+    public String encryption(String password) {
+        return null; //return encrypted password
+    }
+}
