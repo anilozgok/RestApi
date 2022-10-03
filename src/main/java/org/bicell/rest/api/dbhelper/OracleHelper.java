143fb45b3dec615e355d23cdfa435ae7064dc927
Anil Can OZGOK <ozgok1903@gmail.com>
2022-10-03 23:29:24 +0300

    db helpers implemented

--- src/main/java/org/bicell/rest/api/dbhelper/OracleHelper.java
+++ src/main/java/org/bicell/rest/api/dbhelper/OracleHelper.java
@@ -1,4 +1,25 @@
 package org.bicell.rest.api.dbhelper;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.TimeZone;
+
 public class OracleHelper {
+
+    private final String username="";
+
+    private final String password="";
+
+    private final String db_url="";
+
+
+    public OracleHelper() {
+        TimeZone timeZone=TimeZone.getTimeZone("Asia/Kolkata");
+        TimeZone.setDefault(timeZone);
+    }
+
+    public Connection getConnection() throws SQLException{
+        return DriverManager.getConnection(db_url, username, password);
+    }
 }
--- src/main/java/org/bicell/rest/api/dbhelper/VoltDbHelper.java
+++ src/main/java/org/bicell/rest/api/dbhelper/VoltDbHelper.java
@@ -1,4 +1,25 @@
 package org.bicell.rest.api.dbhelper;
 
+import org.voltdb.client.Client;
+import org.voltdb.client.ClientConfig;
+import org.voltdb.client.ClientFactory;
+
+import java.io.IOException;
+
 public class VoltDbHelper {
+
+    private final String db_url="";
+
+    private final int port=0;
+
+    public Client client() throws IOException{
+        Client client;
+        ClientConfig clientConfig;
+        clientConfig=new ClientConfig("advent", "xYZZy");
+        client = ClientFactory.createClient(clientConfig);
+        client.createConnection(db_url, port);
+        return client;
+    }
+
+
 }
