d40fad1d2508fc87c972b4cb8c396f41c3ffa27c
Anil Can OZGOK <ozgok1903@gmail.com>
2022-10-03 23:32:22 +0300

    Update LoginRepository.java

--- src/main/java/org/bicell/rest/api/repository/LoginRepository.java
+++ src/main/java/org/bicell/rest/api/repository/LoginRepository.java
@@ -1,22 +1,28 @@
 package org.bicell.rest.api.repository;
 
+import org.bicell.rest.api.encryption.Encryption;
 import org.bicell.rest.api.entity.Login;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-
 import java.util.logging.Logger;
 
 
 public class LoginRepository {
-    Logger logger=Logger.getLogger(LoginRepository.class.getName());
+    Logger logger = Logger.getLogger(LoginRepository.class.getName());
 
-//TODO login logic will we implemented after db helper implementations. This is a temporary dummy login logic.
-    public ResponseEntity loginCheck(String MSISDN, String password){
-        Login login=new Login(MSISDN,password);
-        if(login.getMSISDN()==password && login.getPassword()==password){
-            logger.info("login successfull");
+    Encryption encryption = new Encryption();
+
+    //TODO login logic will we implemented after oracele helper implementations. This is a temporary dummy login logic.
+    public ResponseEntity loginCheck(String MSISDN, String password) {
+
+        String encryptedPassword=encryption.encryptPassword(password);
+
+        Login login = new Login(MSISDN, password);
+
+        if (login.getMSISDN() == password && login.getPassword() == encryptedPassword) {
+            logger.info("login successful");
             return new ResponseEntity(login, HttpStatus.OK);
-        } else{
+        } else {
             logger.info("login unsuccessful");
             return new ResponseEntity(login, HttpStatus.BAD_REQUEST);
         }
