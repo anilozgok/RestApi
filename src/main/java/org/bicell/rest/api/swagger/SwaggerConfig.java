c2a10156fd2f32f2af05cca388fb4570e34baca0
Anil Can OZGOK <ozgok1903@gmail.com>
2022-10-03 11:52:35 +0300

    Swagger Config done

---
+++ src/main/java/org/bicell/rest/api/swagger/SwaggerConfig.java
@@ -0,0 +1,38 @@
+package org.bicell.rest.api.swagger;
+
+import com.google.common.base.Predicate;
+import org.springframework.context.annotation.*;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.*;
+
+import static springfox.documentation.builders.PathSelectors.regex;
+
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket restApi(){
+
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("org.bicell.rest.api"))
+                .paths((Predicate<String>) regex("/.*"))
+                .build().apiInfo(apiEndPointsInfo());
+    }
+
+    private ApiInfo apiEndPointsInfo(){
+        return new ApiInfoBuilder().title("i2i Rest Api Swagger")
+                .description("Rest Api Documentation")
+                .contact(new Contact("Anıl Can Özgök", "","anil.can.ozgok@ext.i2i-systems.com"))
+                .license("Apache 2.0")
+                .build();
+    }
+
+}
